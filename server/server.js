4bf130b8d695ec37c9863948f7099ce693a270ba
AndrewJoel <megaknight7777777@gmail.com>
2020-05-18 03:47:22 +0530

    Add form for message and show incoming messages

--- server/server.js
+++ server/server.js
@@ -1,11 +1,44 @@
 const path=require('path');
+const http=require('http');
 const express=require('express');
+const socketIO=require('socket.io');
 
 const publicPath=path.join(__dirname,'../public');
-
-var app=express();
 const port=process.env.PORT || 3000;
+var app=express();
+var server=http.createServer(app);
+var io =socketIO(server);
+io.on('connection',(socket)=>{
+    console.log('User connected');
+    socket.emit('newMessage',{
+        from:'Admin',
+        text:'Welcome to the chat',
+        createdAt: new Date().getTime()
+    });
+    socket.broadcast.emit('newMessage',{
+        from:'Admin',
+        text:'New User has joined',
+        createdAt: new Date().getTime()
+    });
+
+    socket.on('createMessage',(message,callback)=>{
+        console.log(`message: ${message}`);
+        io.emit('newMessage',{
+            from:message.from,
+            text:message.text,
+            createdAt: new Date().getTime()
+        });
+        callback('This is from server');
+    });
+
+
+
+
+    socket.on('disconnect',()=>{
+        console.log('User disconnected')
+    });
+});
  app.use(express.static(publicPath)) ;
-  app.listen(port, ()=>{
+ server.listen(port, ()=>{
      console.log(`The port is running at ${port}`);
   });
